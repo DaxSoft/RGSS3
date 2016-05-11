27af2093543e10d996a62952ddd4f35a67eb815a
DaxSoft <dax-soft@live.com>
2016-05-11 14:04:02 -0300

    Update to 2.5

--- Plugin Plus.rb
+++ Plugin Plus.rb
@@ -2,7 +2,7 @@
 # • Plugin
 #==============================================================================
 # Autor: Dax
-# Versão: 2.3
+# Versão: 2.5
 # Site: www.dax-soft.weebly.com
 # Requerimento: Dax Core
 # tiagoms : Ajudou dando feedback.
@@ -47,7 +47,7 @@ Plugin.register(name, author, version, link)
 
 tipos de tags:
   files : Todos os arquivos postos aqui, serão executados.
-  graphics 
+  graphic
   audio 
   movies 
   system
@@ -74,8 +74,12 @@ tipos do HEADER: PASTA PARA:
 #    Add da tag de informação.
 # [2.3]
 #    Novo método de registrar os arquivos do plugin.
+# [2.5]
+#    Correção de pequenos bugs.
+#    Funçao de deletar plugins.
 #==============================================================================
-Dax.register(:plugin, "dax", 2.3, [[:powershell, "dax"]]) {
+Dax.register(:plugin, "dax", 2.5, [[:powershell, "dax"]]) {
+  Dir.mkdir("#{Dir.pwd}/Data/Plugins") unless FileTest.directory?("#{Dir.pwd}/Data/Plugins")
   $ROOT_PATH = ->(filename, dir="") { "#{Dir.pwd}/Data/Plugins/#{dir}#{filename}" }
   #============================================================================
   # • Plugin
@@ -90,17 +94,22 @@ Dax.register(:plugin, "dax", 2.3, [[:powershell, "dax"]]) {
     #--------------------------------------------------------------------------
     # • Constants
     #--------------------------------------------------------------------------
-    VERSION = "2.3"
+    KEY = :F4
+    VERSION = "2.5"
     TEMP = "./temp.txt"
     ERROR = true # mostrar erros.
     MSG = {
-      CONF: "Deseja verificar/baixar novas versões dos plugins?\nCaso seja a primeira vez executando, irá ocorrer um pequeno delay, só aguardar...",
-      DOWN_PLUGIN: "Você deseja baixar o plugin: <%s>, do autor: <%s>, versão: <%03s>",
-      NEW_UPDATE: "Uma nova atualização está disponível, do plugin: <%s>, do autor: <%s>, versão: <%03s>",
+      CONF: "Deseja verificar/baixar novas versões dos plugins?\nCaso seja a primeira vez executando, irá ocorrer um pequeno delay, só aguardar...\nPressione #{KEY} para acessar ao menu.",
+      DOWN_PLUGIN: "Você deseja baixar o plugin: <%s>\nDo autor: <%s>\nversão: <%03s>\n",
+      NEW_UPDATE: "Uma nova atualização está disponível, do plugin: <%s>\ndo autor: <%s>\nversão: <%03s>\n",
       NO_INTERNET: "Sem conexão com à internet",
       FORMAT: MessageBox::ICONINFORMATION|MessageBox::YESNO,
       TITLE: "Plugin Manager #{VERSION}",
-      clear: "Para o sistema de verificação de novas versão funcionar, você deve limpar o cache do seu navegador. Quer fazer isso agora? (Automático)"
+      PLMS: "Plugin Manager #{VERSION} < Plugins Instalados: %02d >",
+      BACK: "Retornar",
+      CL: "<%s> de <%s> versão <%.3f>",
+      DELETE: "Deletar",
+      RF: "Saia do projeto para atualizar o registro.\nDeseja sair agora?",
     }
     RKEY = [:files, :graphic, :system, :audio, :movie, :project]
     #--------------------------------------------------------------------------
@@ -109,12 +118,13 @@ Dax.register(:plugin, "dax", 2.3, [[:powershell, "dax"]]) {
     @@register = {}
     @@download = {}
     @@root_path = {}
-    @@checkExist = false
     #--------------------------------------------------------------------------
     # • Run plugin
     #--------------------------------------------------------------------------
     def run()
+      File.delete(TEMP) if FileTest.exist?(TEMP)
       check()
+      ps("exit")
     end
     #--------------------------------------------------------------------------
     # • Load all plugin files
@@ -126,11 +136,6 @@ Dax.register(:plugin, "dax", 2.3, [[:powershell, "dax"]]) {
           load_script(i) rescue next
         } rescue next
       }
-#~       @@root_path.each_value { |value| 
-#~         value.each { |i| 
-#~           load_script(i) rescue next 
-#~         } 
-#~       }
     end
     #--------------------------------------------------------------------------
     # • Register the plugins
@@ -154,6 +159,7 @@ Dax.register(:plugin, "dax", 2.3, [[:powershell, "dax"]]) {
       @@register[[hash[:name], hash[:author]]] = hash
       @@download[[hash[:name], hash[:author]]] = []
       @@register[[hash[:name], hash[:author]]][:path] = []
+      @@register[[hash[:name], hash[:author]]][:_path] = []
       @@root_path[[hash[:name], hash[:author]]] = []
     end
     #--------------------------------------------------------------------------
@@ -183,6 +189,20 @@ Dax.register(:plugin, "dax", 2.3, [[:powershell, "dax"]]) {
     def registerInfo(key, author, version=nil)
       return unless registred?(key, author, version)
       @@register[[key, author]]
+    end
+    #--------------------------------------------------------------------------
+    # • Get the total of the registers
+    #--------------------------------------------------------------------------
+    def size
+      n = 0
+      @@register.each_value { |v| v[:version] > 0.0 ? n += 1 : next }
+      return n
+    end
+    #--------------------------------------------------------------------------
+    # • Data
+    #--------------------------------------------------------------------------
+    def data
+      @@register
     end
     #--------------------------------------------------------------------------
     # • check file downlaod
@@ -260,6 +280,7 @@ Dax.register(:plugin, "dax", 2.3, [[:powershell, "dax"]]) {
       links = kd.get(:link)
       folder = kd.get(:folder)
       # => files
+      return if files.nil? or files.empty? or !files.is_a?(Array)
       files.each_with_index { |fname, n|
         path = header + folder[n] rescue header
         Dir.mkdir(path) unless FileTest.directory?(path)
@@ -269,6 +290,8 @@ Dax.register(:plugin, "dax", 2.3, [[:powershell, "dax"]]) {
         Graphics.wait(30)
         if rkey == :files
           @@register[[*key]][:path] << fpath
+        else
+          @@register[[*key]][:_path] << fpath
         end
         File.delete(fpath) if FileTest.exist?(fpath)
         File.rename(nfname, fpath) if FileTest.exist?(nfname)
@@ -368,7 +391,7 @@ Dax.register(:plugin, "dax", 2.3, [[:powershell, "dax"]]) {
       list = $1.split(/\n/).if{|i|!i.empty?}.collect!(&:lstrip)
       list.each { |l|
         if l.include?("<f>")
-          l.match(/(.*?)<l>(.*?)<f>(.*)/im)
+          l.match(/(.*?)<l>(.*?)<f>(.*)/i)
           fname = $1.strip rescue ""
           next if fname.nil? or fname.empty?
           unless fname.include?(".")
@@ -381,8 +404,9 @@ Dax.register(:plugin, "dax", 2.3, [[:powershell, "dax"]]) {
           folder = $3.strip rescue ""
           next if folder.nil? or folder.empty?
           hash[:folder] << folder + "/"
+          next
         else
-          l.match(/(.*?)<l>(.*)/im)
+          l.match(/(.*?)<l>(.*)/i)
           fname = $1.strip rescue ""
           next if fname.nil? or fname.empty?
           unless fname.include?(".")
@@ -392,16 +416,184 @@ Dax.register(:plugin, "dax", 2.3, [[:powershell, "dax"]]) {
           link = $2.strip rescue ""
           next if link.nil? or link.empty?
           hash[:link] << link
+          next
         end
       }
       @@hash[tag.symbol].merge!(hash)
     end
   end
   #============================================================================
+  # • Window_PluginManager
+  #============================================================================
+  class Window_PluginManager < Window_Command
+    #--------------------------------------------------------------------------
+    # • Inicialização dos objetos.
+    #--------------------------------------------------------------------------
+    def initialize
+      super(0, 0)
+      self.y += 20
+      self.openness = 0
+      self.opacity = 0
+      self.back_opacity = 0
+      open
+    end
+    #--------------------------------------------------------------------------
+    # • Largura da janela
+    #--------------------------------------------------------------------------
+    def window_width
+      return Graphics.width
+    end
+    #--------------------------------------------------------------------------
+    # • Lista dos comandos
+    #--------------------------------------------------------------------------
+    def make_command_list
+      #add_command(Vocab::to_title, :to_title)
+      add_command(Plugin::MSG[:BACK], :back)
+      Plugin.data.each_pair { |key, value|
+          next if value[:version] <= 0.0
+          str = sprintf(Plugin::MSG[:CL], key[0], key[1], value.get(:version).round(3))
+          skey = ("#{key[0]}_#{key[1]}").symbol
+          add_command(str, skey)
+      } 
+    end
+  end
+  #============================================================================
+  # • Window_PluginManager
+  #============================================================================
+  class Window_PluginManagerConfirm < Window_Command
+    #--------------------------------------------------------------------------
+    # • Inicialização dos objetos.
+    #--------------------------------------------------------------------------
+    def initialize
+      super(0, 0)
+      Position[:center, self]
+      self.openness = 0
+      self.opacity = 0
+      self.back_opacity = 0
+    end
+    #--------------------------------------------------------------------------
+    # • Largura da janela
+    #--------------------------------------------------------------------------
+    def window_width
+      return 164
+    end
+    #--------------------------------------------------------------------------
+    # • Lista dos comandos
+    #--------------------------------------------------------------------------
+    def make_command_list
+      add_command(Plugin::MSG[:DELETE], :delete) 
+      add_command(Plugin::MSG[:BACK], :back)
+    end
+  end
+  #============================================================================
+  # • Scene_PluginManager
+  #============================================================================
+  class Scene_PluginManager < Scene_Base
+    #--------------------------------------------------------------------------
+    # • Inicialização dos objetos.
+    #--------------------------------------------------------------------------
+    def start
+      super
+      @index = []
+      @info = Sprite_Text.new(16, 0, Graphics.width-16, 20, "")
+      @info.size = 18
+      create_window
+      @cwindow = Window_PluginManagerConfirm.new
+      @cwindow.set_handler(:delete, method(:deletePlugin))
+      @cwindow.set_handler(:back, method(:back))
+    end
+    #--------------------------------------------------------------------------
+    # • Criar window
+    #--------------------------------------------------------------------------
+    def create_window
+      oldSize = Font.default_size
+      Font.default_size = 18
+      @window = Window_PluginManager.new
+      @window.set_handler(:back, method(:return_scene))
+      Plugin.data.each_pair { |key, value|
+        next if value[:version] <= 0.0
+        skey = ("#{key[0]}_#{key[1]}").symbol
+        @window.set_handler(skey, method(:open))
+        @index << key
+      }
+      Font.default_size = oldSize
+    end
+    #--------------------------------------------------------------------------
+    # • Open
+    #--------------------------------------------------------------------------
+    def open
+      @cwindow.open
+      @cwindow.active = true
+      @cwindow.visible = true
+      @window.active = false
+    end
+    #--------------------------------------------------------------------------
+    # • Delete
+    #--------------------------------------------------------------------------
+    def deletePlugin
+      current = @index[@window.index.pred]
+      back unless Plugin.registred?(*current)
+      Plugin.registerInfo(*current)[:path].each { |file|
+        puts "Deleting file... #{file}"
+        File.delete(file) if FileTest.exist?(file)
+        puts "File deleted!"
+      }
+      Plugin.registerInfo(*current)[:_path].each { |file|
+        puts "Deleting file... #{file}"
+        File.delete(file) if FileTest.exist?(file)
+        puts "File deleted!"
+      }
+      Plugin.data.delete(current)
+      Plugin.saveRegister()
+      hresult = API::MessageBox.call(Plugin::MSG[:TITLE], Plugin::MSG[:RF], Plugin::MSG[:FORMAT])
+      SceneManager.exit if hresult == API::MessageBox::YES
+      @window.refresh
+      back
+    end
+    #--------------------------------------------------------------------------
+    # • Back
+    #--------------------------------------------------------------------------
+    def back
+      @cwindow.active = false
+      @cwindow.visible = false
+      @window.active = true
+    end
+    #--------------------------------------------------------------------------
+    # • Terminação dos objetos. 
+    #--------------------------------------------------------------------------
+    def terminate 
+      super
+      @window.dispose
+      @cwindow.dispose
+      @info.dispose
+    end
+    #--------------------------------------------------------------------------
+    # • Atualização dos objetos.
+    #--------------------------------------------------------------------------
+    def update
+      super
+      [:B].each { |key| trigger?(key) {return_scene}  }
+      @info.update
+      @info.text = Plugin::MSG[:PLMS] % Plugin.size
+    end
+  end
+  #============================================================================
+  # • Scene_Base
+  #============================================================================
+  class Scene_Base
+    #--------------------------------------------------------------------------
+    # • Atualização dos objetos.
+    #--------------------------------------------------------------------------
+    alias :pluginManagerScene_update :update
+    def update(*args, &block)
+      pluginManagerScene_update(*args, &block)
+      trigger?(Plugin::KEY) { SceneManager.call(Scene_PluginManager) }
+    end
+  end
+  #============================================================================
   # • Insira aqui: Os registros dos plugin.
   #============================================================================
-  Plugin.register(:hello_world, :dax, 0.0, "http://pastebin.com/raw/N2kusL0N")
-  Plugin.register(:steampunk_hud, :dax, 0.0, "http://pastebin.com/raw/mGjQMB95")
+  
   #============================================================================
   # • Run
   #============================================================================
