0660cef028def46142809cb245aa806748788d3f
Dax Soft <dax-soft@live.com>
2017-10-26 18:34:54 -0200

    DSi 0.3

---
+++ dsi_plugin.rb
@@ -0,0 +1,39 @@
+#* Configuração geral *#
+
+Name: DSi
+Version: 0.3
+Size: ~1 MB
+<new>Info about the current version:
+Basic version of iDual Screen for Rpg Maker</new>
+Website: www.dax-soft.weebly.com
+Date: 21/05/2016
+#* Informações do plugin para o PluginManager *#
+
+/*Created by: Dax
+Date: 26/10/2017
+Any bug or whatever, contact-me
+DSi is about of a script pack that will make your rpg maker game is look like a nintendo DS game. 
+*/
+
+#* Todos os arquivos dentro do file, serão executados *#
+<files>
+o: rb
+folder: DSi
+dsi <l: https://pastebin.com/raw/x4Y5batR> 
+dsi_title <l: https://pastebin.com/raw/75fGaGVY>
+dsi_submenu <l: https://pastebin.com/raw/bDtZmDNP>
+dsi_menu <l: https://pastebin.com/raw/MeTmn427>
+</files>
+
+#* Thumbnail *#
+<graphic>
+output: png
+folder: System
+# msgbox <l: https://i.imgur.com/woeI6wK.png>
+# MenuBackground <l: https://i.imgur.com/eU3D6Ge.png>
+# Grassland <l: https://i.imgur.com/Y6IIWiE.png>
+# savebutton <l: https://i.imgur.com/lhzKzqd.png>
+# backdown <l: https://i.imgur.com/eU3D6Ge.png> <f: System/Title>
+# backup <l: https://i.imgur.com/T92fX4w.png> <f: System/Title>
+# logo <l: https://i.imgur.com/v6QT4uY.png> <f: System/Title>
+</graphic>
