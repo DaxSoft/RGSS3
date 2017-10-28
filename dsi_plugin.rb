a0b857f6af94de44f0611ae6f1c4dc7408073e49
Dax Soft <dax-soft@live.com>
2017-10-28 15:39:59 -0200

    Update dsi_plugin.rb

--- dsi_plugin.rb
+++ dsi_plugin.rb
@@ -1,7 +1,7 @@
 #* Configuração geral *#
 
 Name: DSi
-Version: 0.3
+Version: 0.32
 Size: ~1 MB
 <new>Info about the current version:
 Basic version of iDual Screen for Rpg Maker</new>
@@ -23,17 +23,19 @@ dsi <l: https://pastebin.com/raw/x4Y5batR>
 dsi_title <l: https://pastebin.com/raw/75fGaGVY>
 dsi_submenu <l: https://pastebin.com/raw/bDtZmDNP>
 dsi_menu <l: https://pastebin.com/raw/MeTmn427>
+dsi_cursor <l: https://pastebin.com/raw/X8n0NSz5>
 </files>
 
 #* Thumbnail *#
 <graphic>
 output: png
 folder: System
- msgbox <l: http://i.imgur.com/woeI6wK.png>
- MenuBackground <l: http://i.imgur.com/eU3D6Ge.png>
- Grassland <l: http://i.imgur.com/Y6IIWiE.png>
- savebutton <l: http://i.imgur.com/lhzKzqd.png>
- backdown <l: http://i.imgur.com/eU3D6Ge.png> <f: System/Title>
- backup <l: http://i.imgur.com/T92fX4w.png> <f: System/Title>
- logo <l: http://i.imgur.com/v6QT4uY.png> <f: System/Title>
+# msgbox <l: http://i.imgur.com/woeI6wK.png>
+# MenuBackground <l: http://i.imgur.com/eU3D6Ge.png>
+# Grassland <l: http://i.imgur.com/Y6IIWiE.png>
+# savebutton <l: http://i.imgur.com/lhzKzqd.png>
+# backdown <l: http://i.imgur.com/eU3D6Ge.png> <f: System/Title>
+# backup <l: http://i.imgur.com/T92fX4w.png> <f: System/Title>
+# logo <l: http://i.imgur.com/v6QT4uY.png> <f: System/Title>
+# cursor <l: http://i.imgur.com/AgZHGHl.png>  
 </graphic>
